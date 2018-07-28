70b882e377ddea903f55cbeb38cffa834dc5d7c1
koalasurvey <rilts@koalasurveyui-MacBook.local>
2018-07-28 16:03:33 +0900

    display a data 반복연산자

---
+++ js/es5/for_in.js
@@ -0,0 +1,15 @@
+var hero = {
+  "id":1,
+  "name":"kim",
+};
+
+for(var h in hero) {
+  console.log(h); // h = 'id'
+}
+
+var fruits = ['apple', 'banana', 'grape'];
+
+for(var item in fruits) {
+  console.log(item);
+}
+
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -19,6 +20,7 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Winstorm'
   };
+  heroes = HEROES;
 
   constructor() { }
 
