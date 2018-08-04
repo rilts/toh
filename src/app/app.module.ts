de247c59392a1624e29181c1bd26b86e2e07766a
Seungjune Kwak <rialtoksj@yahoo.com>
2018-08-04 11:22:54 +0900

    pass data to parent from child

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { VotetakerComponent } from './votetaker/votetaker.component';
+import { VoterComponent } from './voter/voter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    VotetakerComponent,
+    VoterComponent
   ],
   imports: [
     BrowserModule,
